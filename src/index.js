2a605c9254c72e6fbb61b3187efe6294dd21849b
devilknock <deviamita007@gmail.com>
2025-11-26 00:52:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVita>
+import reportWebVitals from './reportWebVi8tals';
 
-const root = ReactDOM.createRoot(document.ge>
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performanc>
-// to log results (for example: reportWebVit>
-// or send to an analytics endpoint. Learn m>
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
